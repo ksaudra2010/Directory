a64bec48bbbce3c83bcfd5f02c612f198ea4ee21
Audra A Kosinski <57331762+ksaudra2010@users.noreply.github.com>
2020-04-18 22:15:22 -0500

    Update index.js

--- src/index.js
+++ src/index.js
@@ -2,4 +2,6 @@ import React from "react";
 import ReactDOM from "react-dom";
 import App from "./App";
 
+const PORT = process.env.PORT || 3000
+
 ReactDOM.render(<App />, document.getElementById("root"));
