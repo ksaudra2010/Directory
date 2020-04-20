e05d878d1d2de41c6a8eb49366793ab5745348fd
Audra A Kosinski <57331762+ksaudra2010@users.noreply.github.com>
2020-04-19 22:08:51 -0500

    Updated PORT

--- src/index.js
+++ src/index.js
@@ -2,6 +2,8 @@ import React from "react";
 import ReactDOM from "react-dom";
 import App from "./App";
 
-const PORT = process.env.PORT || 3000
+app.listen(process.env.PORT || 3000, function(){
+  console.log("Express server listening on port %d in %s mode", this.address().port, app.settings.env);
+});
 
 ReactDOM.render(<App />, document.getElementById("root"));
