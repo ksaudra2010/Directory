f9737ff1992140fbfe3f49cac796076bb20530fa
Audra A Kosinski <57331762+ksaudra2010@users.noreply.github.com>
2020-04-22 20:36:44 -0500

    Delete index.js

--- src/index.js
+++
@@ -1,7 +0,0 @@
-import React from "react";
-import ReactDOM from "react-dom";
-import App from "./App";
-
-var PORT = process.env.PORT || 3000;
-
-ReactDOM.render(<App />, document.getElementById("root"));
