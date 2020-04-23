d5073b009fa92f2378c36aa111a40780c66ae25d
Audra A Kosinski <57331762+ksaudra2010@users.noreply.github.com>
2020-04-22 20:38:36 -0500

    Updated index.js

---
+++ src/index.js
@@ -0,0 +1,7 @@
+import React from "react";
+import ReactDOM from "react-dom";
+import App from "./App";
+
+var PORT = process.env.PORT || 3000;
+
+ReactDOM.render(<App />, document.getElementById("root"));
